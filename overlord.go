fd52c352deaddcd2d0c3150974c800a70f8e6822
Your Name <you@example.com>
2018-02-21 13:51:03 -0300

    Moved to modular design

--- overlord.go
+++ overlord.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	//"bytes"
 	"fmt"
+	//"io/ioutil"
 	"net"
 	"net/url"
 	//"regexp"
@@ -15,118 +17,180 @@ import (
 	"github.com/1lann/cete"
 	"github.com/gocolly/colly"
 	"github.com/gocolly/colly/debug"
+	"github.com/gocolly/colly/proxy"
 )
 
-type Server struct {
-	URL       string
-	Headers   string
-	UserAgent string
-	// Use other tools to get these
-	IpAddress      string
-	OpenPorts      []int
-	WebApplication string
-	Banner         string
-	CrawledAt      time.Time
+///// App ///////////////////////////////////////////////
+type App struct {
+	Name string
+	Version
+	Collector *colly.Collector
+	Database
 }
 
-func panicNotNil(err error) {
-	if err != nil {
-		panic(err)
-	}
-}
-
-func main() {
-	// Initialize Servers DB
-	db, _ := cete.Open("./server_data")
-	defer db.Close()
-	db.NewTable("servers")
-	db.Table("servers").NewIndex("URL")
-
-	// Instantiate default collector
-	c := colly.NewCollector(
-		colly.IgnoreRobotsTxt(),
-		colly.UserAgent("Mozilla/5.0 (X11; Linux x86_64; rv:52.0) Gecko/20100101 Firefox/52.0"),
-		colly.Debugger(&debug.LogDebugger{}),
-		colly.MaxDepth(15),
-		colly.Async(true),
-		//colly.URLFilters(
-		//	regexp.MustCompile("(.+|^facebook|^twitter)$"),
-		//),
-	)
-	//rp, err := proxy.RoundRobinProxySwitcher("socks5://127.0.0.1:1337", "socks5://127.0.0.1:1338")
-	//if err != nil {
-	//	log.Fatal(err)
-	//}
-	//c.SetProxyFunc(rp)
-
-	c.Limit(&colly.LimitRule{
-		Parallelism: 2,
-		RandomDelay: 5 * time.Second,
+func (self App) SetCollectorLimits(parallelism int, delay time.Duration) {
+	self.Collector.Limit(&colly.LimitRule{
+		Parallelism: parallelism,
+		RandomDelay: delay * time.Second,
 	})
+}
 
-	// On every a element which has href attribute call callback
-	c.OnHTML("a[href]", func(e *colly.HTMLElement) {
+///// Version //////////////////////////////////////////
+type Version struct {
+	Major int
+	Minor int
+	Patch int
+}
+
+func (self Version) String() string {
+	return fmt.Sprintf("%v.%v.%v", self.Major, self.Minor, self.Patch)
+}
+
+///// Database /////////////////////////////////////////
+type Database struct {
+	Data cete.DB
+	// Cached
+	Servers     *cete.Range
+	ServerCount int64
+}
+
+func InitDB(path string, table string, indices []string) (db cete.DB) {
+	// Load Database
+	ptr, _ := cete.Open(path)
+	db = *(ptr)
+	// Initialize Servers DB
+	db.NewTable(table)
+	// Initialize DB Indices
+	for _, index := range indices {
+		db.Table(table).NewIndex(index)
+	}
+	return db
+}
+
+func (self App) SetCollectorProxies(socksProxies string) (err error) {
+	proxies, err := proxy.RoundRobinProxySwitcher(socksProxies)
+	if err == nil {
+		self.Collector.SetProxyFunc(proxies)
+	}
+	return err
+}
+
+func (self Database) CacheServers() *cete.Range {
+	// Cache Server Data
+	self.Servers = self.Data.Table("servers").All()
+	self.ServerCount, _ = self.Servers.Count()
+	return self.Servers
+}
+
+func (self Database) PrintServers() {
+	fmt.Println("  Server Count: ", self.ServerCount)
+	fmt.Println("=================================================")
+	self.Servers.Do(func(k string, c uint64, d cete.Document) error {
+		var s Server
+		d.Decode(&s)
+		fmt.Println("-------------")
+		fmt.Println("Key:         ", k)
+		fmt.Println("Counter:     ", c)
+		fmt.Println("-------------")
+		fmt.Println("URL:         ", s.URL)
+		fmt.Println("User-Agent:  ", s.UserAgent)
+		fmt.Println("Headers:     ", s.Headers)
+		fmt.Println("-------------")
+		return nil
+	}, 1)
+}
+
+func (self Database) InsertServer(s Server) error {
+	return self.Data.Table("servers").Set(s.Host, Server{})
+}
+
+///// Server ///////////////////////////////////////////
+type Server struct {
+	CrawledAt   time.Time
+	URL         string
+	Scheme      string
+	Host        string
+	Path        string
+	RawQuery    string
+	Headers     string
+	Body        string
+	UserAgent   string
+	IPAddress   string
+	IPAddresses []string
+	// Use other tools to get these
+	//OpenPorts      []int
+	//WebApplication string
+	//Banner         string
+}
+
+///// Main ////////////////////////////////////////////
+func main() {
+	app := App{
+		Name: "Overlord",
+		Database: Database{
+			Data: InitDB("./servers_db/", "servers", []string{"Host", "IPAddress"}),
+		},
+		Version: Version{0, 1, 0},
+		Collector: colly.NewCollector(
+			colly.Debugger(&debug.LogDebugger{}),
+			colly.IgnoreRobotsTxt(),
+			colly.MaxDepth(15),
+			colly.CacheDir("./_overload_cache/"),
+			colly.DisallowedDomains("facebook.com", "twitter.com"),
+			colly.UserAgent("Mozilla/5.0 (X11; Linux x86_64; rv:52.0) Gecko/20100101 Firefox/52.0"),
+			//colly.UserAgent("Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36"),
+			//colly.Async(true),
+			//colly.URLFilters(
+			//	regexp.MustCompile("(.+|^facebook|^twitter)$"),
+			//),
+		),
+	}
+	defer app.Database.Data.Close()
+	app.SetCollectorLimits(2, 4)
+	///////////////////////////////////////////////////////////////
+	// On HTML Load ///////////////////////////////////////////////
+	app.Collector.OnHTML("a[href]", func(e *colly.HTMLElement) {
 		link := e.Attr("href")
 		fmt.Println(link)
 		e.Request.Visit(link)
 	})
-
-	c.OnRequest(func(r *colly.Request) {
-		headersString := fmt.Sprintf("%s", r.Headers)
-		db.Table("servers").Set(r.URL.String(), Server{
-			URL:       r.URL.String(),
-			Headers:   headersString[5:(len(headersString) - 1)],
-			UserAgent: r.Headers.Get("User-Agent"),
-			CrawledAt: time.Now(),
-		})
-		fmt.Println("Headers: ", r.Headers)
-		fmt.Println("Visiting", r.URL)
-		fmt.Println("Body: ", r.Body)
-		//fmt.Println("%s\n", bytes.Replace(r.Body, []byte("\n"), nil, -1))
-		fmt.Println("=================================================")
-		servers := db.Table("servers").All()
-		serverCount, _ := servers.Count()
-		fmt.Println("  Server Count: ", serverCount)
-		fmt.Println("=================================================")
-		db.Table("servers").All().Do(func(key string, counter uint64, d cete.Document) error {
-			var s Server
-			d.Decode(&s)
-			//fmt.Println("d.Key():     ", d.Key())
-			//fmt.Println("d.Counter(): ", d.Counter())
-			fmt.Println("URL:         ", s.URL)
-			fmt.Println("User-Agent:  ", s.UserAgent)
-			fmt.Println("Headers:     ", s.Headers)
-
-			u, err := url.Parse(s.URL)
-			if err != nil {
-				fmt.Println("Error: ", err)
+	///////////////////////////////////////////////////////////////
+	// On Request /////////////////////////////////////////////////
+	app.Collector.OnRequest(func(r *colly.Request) {
+		if len(r.URL.String()) > 0 {
+			// Parse URL
+			u, _ := url.Parse(r.URL.String())
+			// Parse Addresses
+			addresses, _ := net.LookupIP(u.Host)
+			var stringAddresses []string
+			for _, address := range addresses {
+				stringAddresses = append(stringAddresses, address.String())
 			}
-			addresses, err := net.LookupIP(u.Host)
-			if err != nil {
-				fmt.Println("Error: ", err)
-			}
-			fmt.Println("Addresses found at URL: ", addresses)
-			//fmt.
-
-			return nil
-		}, 1)
-
-		fmt.Println("=================================================")
+			// Parse Headers
+			headersString := fmt.Sprintf("%s", r.Headers)
+			// Add Server
+			app.Database.Data.Table("servers").Set(u.Host, Server{
+				Host:        u.Host,
+				URL:         r.URL.String(),
+				Scheme:      u.Scheme,
+				Path:        u.Path,
+				RawQuery:    u.RawQuery,
+				IPAddress:   addresses[0].String(),
+				IPAddresses: stringAddresses,
+				Headers:     headersString[5:(len(headersString) - 1)],
+				UserAgent:   r.Headers.Get("User-Agent"),
+				CrawledAt:   time.Now(),
+			})
+			fmt.Println("=================================================")
+		}
 	})
-
-	//c.OnResponse(func(r *colly.Response) {
+	//app.Collector.OnResponse(func(r *colly.Response) {
 	//	fmt.Println(r.Ctx.Get("url"))
 	//})
 	//// Set error handler
-	//c.OnError(func(r *colly.Response, err error) {
+	//app.Collector.OnError(func(r *colly.Response, err error) {
 	//	fmt.Println("Request URL:", r.Request.URL, "failed with response:", r, "\nError:", err)
 	//})
-	// Start scraping market index of coinmarketcap to find Markets
-	//
-	// Print out saved servers
-	//////////////////////////
-
 	// Seed
-	c.Visit("https://coinmarketcap.com/currencies/volume/24-hour/")
-
+	app.Collector.Visit("https://coinmarketcap.com/currencies/volume/24-hour/")
 }
